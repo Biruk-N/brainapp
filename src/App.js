7f1a6ec273d319d2eafd9c95bfcc950a5f6f6bfb
Biruk <86155266+Biruk-N@users.noreply.github.com>
2021-11-07 11:59:05 +0300

    Update App.js
    clarifii restore

--- src/App.js
+++ src/App.js
@@ -32,30 +32,6 @@ onButtonSubmit = () =>{
   console.log('click');
  
 
-  stub.PostModelOutputs(
-    {
-        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-        model_id: "f76196b43bbd45c99b4f3cd8e8b40a8a",
-        inputs: [{data: {image: {url: "https://samples.clarifai.com/dog2.jpeg"}}}]
-    },
-    metadata,
-    (err, response) => {
-        if (err) {
-            console.log("Error: " + err);
-            return;
-        }
-
-        if (response.status.code !== 10000) {
-            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-            return;
-        }
-
-        console.log("Predicted concepts, with confidence values:")
-        for (const c of response.outputs[0].data.concepts) {
-            console.log(c.name + ": " + c.value);
-        }
-    }
-);
 }
 
 
