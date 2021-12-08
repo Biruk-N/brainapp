8e231d535c88a200a1c09af036d294d6a2f6804b
biruk <birubu178@gmail.com>
2021-12-08 21:57:04 +0300

    edit

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Signin = () =>{
 	return (
 		
-            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow5 center">
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
             <form className="measure ">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -22,7 +22,7 @@ const Signin = () =>{
                 <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                 </div>
                 <div className="lh-copy mt3">
-                <a href="#0" className="f6 link dim black db">Sign up</a>
+                <a href="#0" className="f6 link dim black db">Register</a>
               
                 </div>
             </form>
