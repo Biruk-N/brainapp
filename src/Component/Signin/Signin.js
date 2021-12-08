2576667351de5051437881b71b7e285915bbc1e5
biruk <birubu178@gmail.com>
2021-12-08 22:00:45 +0300

    edit

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -7,7 +7,7 @@ const Signin = () =>{
             <main className="pa4 black-80">
             <form className="measure ">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3">
                     <label className="db fw6 lh-copy f6" for="email-address">Email</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
