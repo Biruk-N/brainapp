00e6f2887c433d57421b55ecdc1b7790c7f6128b
biruk <birubu178@gmail.com>
2021-12-08 21:55:29 +0300

    edit

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Signin = () =>{
 	return (
 		
-            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow5 center">
             <main className="pa4 black-80">
             <form className="measure ">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
