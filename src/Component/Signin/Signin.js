cdee687e86ca5ff187067d62a1c6e8169078dd84
biruk <birubu178@gmail.com>
2021-12-08 21:37:30 +0300

    Signin

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Signin = ({ onInputChange, onButtonSubmit }) =>{
+const Signin = () =>{
 	return (
 		<div className='center'>
-            <img alt='' src={'https://thumbs.dreamstime.com/z/beautiful-face-young-woman-health-fresh-skin-perfect-eyes-beautiful-face-young-woman-health-fresh-skin-157476477.jpg'}/>
-		</div>
+            
+    	</div>
 		);
 }
 
