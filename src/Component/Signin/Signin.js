5aa575ff9bb92512681a8ce1bde244204732f6a5
biruk <birubu178@gmail.com>
2021-12-08 21:36:29 +0300

    Signin

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const FaceRecognition = ({ onInputChange, onButtonSubmit }) =>{
+const Signin = ({ onInputChange, onButtonSubmit }) =>{
 	return (
 		<div className='center'>
             <img alt='' src={'https://thumbs.dreamstime.com/z/beautiful-face-young-woman-health-fresh-skin-perfect-eyes-beautiful-face-young-woman-health-fresh-skin-157476477.jpg'}/>
@@ -8,4 +8,4 @@ const FaceRecognition = ({ onInputChange, onButtonSubmit }) =>{
 		);
 }
 
-export default FaceRecognition;
+export default Signin;
