2526561bd015dcdeea5d1a706813a959281b8f37
Biruk <>
2021-12-16 00:09:10 +0300

    signin input

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Signin = () =>{
+const Signin = ({onRouteChange}) =>{
 	return (
 		
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -21,7 +21,7 @@ const Signin = () =>{
                 </fieldset>
                 <div className="">
                 <input 
-
+                    onClick
                     className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                     type="submit" 
                     value="Sign in" 
