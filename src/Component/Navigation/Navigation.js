57de9afe5270bcf4d3f46f944085deb07bb09fc1
Biruk <>
2021-12-16 09:12:31 +0300

    .

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
-const Navigation = ({onRoutechange}) =>{
+// onRoutechange
+const Navigation = ({}) =>{
 	return (
-		<nav style = {{display: 'flex', justifyContent: 'flex-end'}} onClick={onRouteChange}>
+		<nav style = {{display: 'flex', justifyContent: 'flex-end'}} >
 			<p className = 'f3 link dim black underline pa3 pointer'>Sign Out</p>
 		</nav>
 		)
 }
-
+// onClick={onRouteChange}
 export default Navigation;
