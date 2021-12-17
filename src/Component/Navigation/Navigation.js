84cc9691cd72089b1ac92397f1a3d9aeeedbe171
Biruk <>
2021-12-17 22:03:34 +0300

    add condition to nav

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -2,9 +2,19 @@ import React from 'react';
 // 
 const Navigation = ({onRouteChange}) =>{
 	return (
-		<nav style = {{display: 'flex', justifyContent: 'flex-end'}} >
+		if (isSignedIn) {
+			<nav style = {{display: 'flex', justifyContent: 'flex-end'}} >
 		 <p onClick={()=> onRouteChange('Signin')} className = 'f3 link dim black underline pa3 pointer'>Sign Out</p>
 		</nav>
+		}else{
+			<nav style = {{display: 'flex', justifyContent: 'flex-end'}} >
+			<p onClick={()=> onRouteChange('Signin')} className = 'f3 link dim black underline pa3 pointer'>Sign in</p>
+			</nav>
+			<nav style = {{display: 'flex', justifyContent: 'flex-end'}} >
+			<p onClick={()=> onRouteChange('Signin')} className = 'f3 link dim black underline pa3 pointer'>Register</p>
+			</nav>
+					
+		}
 		)
 }
 
