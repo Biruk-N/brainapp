6450fe6f2d0781d4e787d2b96bf29acf0a7ecfae
Biruk <>
2021-12-17 22:00:24 +0300

    .

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -29,7 +29,7 @@ const Signin = ({onRouteChange}) =>{
                 />
                 </div>
                 <div className="lh-copy mt3">
-                <p   onClick={() => onRouteChange('Register')} className="f6 link dim black db">Register</p>
+                <p   onClick={() => onRouteChange('Register')} className="f6 link dim black db pointer">Register</p>
               
                 </div>
             </form>
